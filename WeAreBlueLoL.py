cde609590b3be9c9764cdc341e27ea9e0ffa1bbc
leesh06289 <67326233+leesh06289@users.noreply.github.com>
2020-12-02 19:17:27 +0900

    Update WeAreBlueLoL.py

--- WeAreBlueLoL.py
+++ WeAreBlueLoL.py
@@ -260,5 +260,5 @@ async def on_message(message): # on_message() event : when the bot has recieved 
             await message.channel.send("Error : Non existing Summoner ", embed=embed)
 
 
-access_token = os.environ["BOT_TOKEN"]
-client.run(access_token)
+access_token = os.environ['BOT_TOKEN']
+client.run()
